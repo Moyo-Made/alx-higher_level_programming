0798c8c07990e2bc1472fbd0a311f38b9a993079
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-06-15 07:59:05 -0700

    a function that checks for lowercase character.

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if "a" <= c <= "z":
+    if len(c) != 1:
+        raise valueError("Input must be a single character")
+    elif "a" <= c <= "z":
         return True
     else:
         return False
